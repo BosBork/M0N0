d211b7c173a9df88593362d6552d2c3c5e4ab3cb
BBosan <60742824+BBosan@users.noreply.github.com>
2021-11-29 13:29:39 +0100

    Added EFModels and installed System.ComponentModel.DataAnnotations

---
+++ EntitiesCL/EFModels/VehicleMake.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntitiesCL.EFModels
+{
+    public class VehicleMake
+    {
+        public int VehicleMakeId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(10)]
+        public string Abrv { get; set; }
+
+        public ICollection<VehicleModel> VehicleModels { get; set; }
+    }
+}
---
+++ EntitiesCL/EFModels/VehicleModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntitiesCL.EFModels
+{
+    public class VehicleModel
+    {
+        public int VehicleModelId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(10)]
+        public string Abrv { get; set; }
+
+        public int VehicleMakeId { get; set; }
+        public virtual VehicleMake VehicleMake { get; set; }
+    }
+}
