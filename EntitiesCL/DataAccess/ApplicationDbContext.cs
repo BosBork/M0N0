70e580186568d2e307dbcc02a0a9f7642afe868d
BBosan <60742824+BBosan@users.noreply.github.com>
2021-11-29 13:33:21 +0100

    Added ApplicationDbContext class and installed Microsoft.EntityFrameworkCore

---
+++ EntitiesCL/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EntitiesCL.EFModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntitiesCL.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<VehicleMake> VehicleMakes { get; set; }
+        public virtual DbSet<VehicleModel> VehicleModels { get; set; }
+
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
