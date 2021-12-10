2bb1660285844da4324e42160efe377b7897e277
BBosan <60742824+BBosan@users.noreply.github.com>
2021-12-10 18:57:45 +0100

    Added Mapping to MappingProfile

--- UI/MappingProfile.cs
+++ UI/MappingProfile.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UI.ViewModels;
 
 namespace UI
 {
@@ -20,6 +21,15 @@ namespace UI
             CreateMap<VehicleModel, VehicleModelDTO>();
             CreateMap<VehicleModelCreateDTO, VehicleModel>();
             CreateMap<VehicleModelUpdateDTO, VehicleModel>();
+
+
+
+            CreateMap<VehicleModel, VehicleModelVM>();
+            CreateMap<VehicleModelDTO, VehicleModelVM>();
+
+            CreateMap<VehicleMake, VehicleMakeVM>();
+            CreateMap<VehicleMakeDTO, VehicleMakeVM>();
+
         }
     }
 }
