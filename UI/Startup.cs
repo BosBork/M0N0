e1ef92f7f724223a6486986720ef502a9d7b2e05
BBosan <60742824+BBosan@users.noreply.github.com>
2021-12-12 20:26:15 +0100

    Added Search input to index view

--- UI/Startup.cs
+++ UI/Startup.cs
@@ -72,7 +72,7 @@ namespace UI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Makes}/{action=Index}/{id?}");
             });
 
             //app.UseEndpoints(endpoints =>
