635d7d78a1cad7687ca740a63e6299c70fb16dd8
BBosan <60742824+BBosan@users.noreply.github.com>
2021-12-18 21:24:44 +0100

    fin

--- UI/Startup.cs
+++ UI/Startup.cs
@@ -72,7 +72,7 @@ namespace UI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Makes}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
 
             //app.UseEndpoints(endpoints =>
